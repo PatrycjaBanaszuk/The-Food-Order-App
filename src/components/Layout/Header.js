64660934c4d59a447b317bd6d0201c5db67943a6
Patrycja Banaszuk <pwbanaszuk@gmail.com>
2022-09-08 19:45:28 +0100

    header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import { Fragment } from 'react'
+import mealsImage from '../../assests/meals.jpg'
+import classes from './Header.module.css'
+
+const Header = props => {
+	<Fragment>
+		<header className={classes.header}>
+			<h1>Meals</h1>
+			<button>Cart</button>
+		</header>
+		<div className={classes['main-image']}>
+			<img src={mealsImage} alt='A table full of delicious food!' />
+		</div>
+	</Fragment>
+}
+
+export default Header
