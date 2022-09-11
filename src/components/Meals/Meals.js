6d0b6e2eeee80c22c47f6b8fbb4bc20391c3ebee
Patrycja Banaszuk <pwbanaszuk@gmail.com>
2022-09-11 13:58:18 +0100

    Modal added

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,12 +1,14 @@
-import { Fragment } from "react";
-import AvailableMeals from "./AvailableMeals";
-import MealsSummary from "./MealsSummary";
+import { Fragment } from 'react'
+import AvailableMeals from './AvailableMeals'
+import MealsSummary from './MealsSummary'
 
 const Meals = () => {
-  return <Fragment>
-        <AvailableMeals />
-        <MealsSummary />
-    </Fragment>
+	return (
+		<Fragment>
+			<MealsSummary />
+			<AvailableMeals />
+		</Fragment>
+	)
 }
 
 export default Meals
